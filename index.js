d79dc4a41b6bd6b8b06cb4f3a4c394264d9fe891
Brett B <65505937+BreadBooks@users.noreply.github.com>
2020-09-23 18:45:07 -0500

    testing

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markDown = require("./utils/generateMarkDown")
 
+// array of questions for user
+const questions = [{
+    type: "",
+    name: "title",
+    message: "What is the name of your project?",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, markDown(data), function(error) {
+        if (error) {
+            console.log("Error Occured", error);
+        } else {
+            console.log("ReadME Created.")
+        }
+    });
 }
 
 // function to initialize program
 function init() {
-
+inquirer
+.prompt(questions)
+.then(function (data) {
+    writeToFile("README.md", data)
+});
 }
 
 // function call to initialize program
